f4028635a5b74b4d10ff6b77fee6a32276a2528e
Nishtha <2023mt93239@wilp.bits-pilani.ac.in>
2024-04-27 00:09:00 +0530

    componentfolder added

---
+++ src/components/navbar.js
@@ -0,0 +1,15 @@
+import React from "react";
+const navbar = () => {
+    return (
+        <nav>
+            <div className="nav_box">
+                <span className="book_app">
+                    Book Store
+                </span>
+                <li></li>
+            </div>
+        </nav>
+    )
+      
+}
+export default navbar
